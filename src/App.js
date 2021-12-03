db0501a175dbe95172a9654674fba8f960233390
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-12-03 07:33:57 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './data.json';
+
+const events = ["Berlin Conference", "Hamburg Conference", "Leipzig Meeting", "Munich Party", "Hannover Book Fair"];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		  <h2>There are {data.events.length} events.</h2>
+		  {data.events.map(event => {
+			  return (
+				  <div>{event.title} = {event.signups} signups</div>
+			  )
+		  })}
     </div>
   );
 }
